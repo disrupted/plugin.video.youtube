0e3d7eb2aea2c20847a675978465b49f7b4d21be
disrupted <salomon.popp@gmail.com>
2017-02-05 15:13:42 +0100

    add back "set content type to videos instead of episodes"

--- resources/lib/kodion/constants/const_content_types.py
+++ resources/lib/kodion/constants/const_content_types.py
@@ -7,8 +7,4 @@ ALBUMS = 'albums'
 MOVIES = 'movies'
 TV_SHOWS = 'tvshows'
 EPISODES = 'episodes'
-<<<<<<< HEAD
-=======
-VIDEOS = 'videos'
->>>>>>> 60b532f8568e1621ddbed930086bd9e1cfba151b
 MUSIC_VIDEOS = 'musicvideos'
